a798f892fd4d3db8458193d4495d01be8db7e6ad
offenguthub <kennedy.bruce@offenpetro.com>
2023-11-08 21:15:30 -0600

    updated all the code to reflect the boomi-app

--- src/component.py
+++ src/component.py
@@ -5,73 +5,93 @@ Template Component main class.
 import csv
 import logging
 from datetime import datetime
+import requests
+import json
+import os 
+
 
 from keboola.component.base import ComponentBase
 from keboola.component.exceptions import UserException
 
 # configuration variables
-KEY_API_TOKEN = '#api_token'
-KEY_PRINT_HELLO = 'print_hello'
+KEY_USERNAME = 'username'
+KEY_PASSWORD = 'password'
+KEY_PROCESS_ID = 'process_id'
+KEY_ATOM_ID = 'atom_id'
+KEY_URL = 'url'
 
 # list of mandatory parameters => if some is missing,
 # component will fail with readable message on initialization.
-REQUIRED_PARAMETERS = [KEY_PRINT_HELLO]
+REQUIRED_PARAMETERS = [KEY_USERNAME,KEY_PASSWORD,KEY_PROCESS_ID,KEY_ATOM_ID, KEY_URL]
 REQUIRED_IMAGE_PARS = []
 
 
+def trigger_job(url: str,username: str, password: str, process_id: str, atom_id: str):
+    """Triggers a Boomi job.
+
+  Args:
+    url: The URL of the Boomi API endpoint.
+    username: The Boomi username.
+    password: The Boomi password.
+    process_id: The ID of the Boomi process to trigger.
+    atom_id: The ID of the Boomi atom to trigger.
+
+  Returns:
+    The response text from the Boomi API.
+  """
+
+    # Define the API endpoint and URL
+    url = url
+
+    # Define the XML request body
+    xml_request_body = f"""
+    <ExecutionRequest processId="{process_id}" atomId="{atom_id}" xmlns="http://api.platform.boomi.com/">
+    </ExecutionRequest>
+    """
+
+    # Define headers with Basic Authentication
+    headers = {
+        "Content-Type": "application/xml"
+    }
+    auth = (username, password)
+
+    # Send the HTTP POST request
+    response = requests.post(url, headers=headers, data=xml_request_body, auth=auth)
+
+    # Check the response
+    if response.status_code == 200:
+        print("Request successful")
+        return response.text
+    else:
+        print("Request failed with status code:", response.status_code)
+        return None
+
 class Component(ComponentBase):
-    """
-        Extends base class for general Python components. Initializes the CommonInterface
-        and performs configuration validation.
-
-        For easier debugging the data folder is picked up by default from `../data` path,
-        relative to working directory.
-
-        If `debug` parameter is present in the `config.json`, the default logger is set to verbose DEBUG mode.
-    """
 
     def __init__(self):
         super().__init__()
 
-    def run(self):
-        """
-        Main execution code
+    def run(self) -> None:
+
+        """Runs the component.
+
+        Validates the configuration parameters and triggers a Boomi job.
         """
 
-        # ####### EXAMPLE TO REMOVE
-        # check for missing configuration parameters
+       # check for missing configuration parameters
         self.validate_configuration_parameters(REQUIRED_PARAMETERS)
-        self.validate_image_parameters(REQUIRED_IMAGE_PARS)
-        params = self.configuration.parameters
-        # Access parameters in data/config.json
-        if params.get(KEY_PRINT_HELLO):
-            logging.info("Hello World")
+        
+        username = self.configuration.parameters.get(KEY_USERNAME)
+        password = self.configuration.parameters.get(KEY_PASSWORD)
+        process_id = self.configuration.parameters.get(KEY_PROCESS_ID)
+        url = self.configuration.parameters.get(KEY_URL)
+        atom_id = self.configuration.parameters.get(KEY_ATOM_ID)
 
-        # get last state data/in/state.json from previous run
-        previous_state = self.get_state_file()
-        logging.info(previous_state.get('some_state_parameter'))
-
-        # Create output table (Tabledefinition - just metadata)
-        table = self.create_out_table_definition('output.csv', incremental=True, primary_key=['timestamp'])
-
-        # get file path of the table (data/out/tables/Features.csv)
-        out_table_path = table.full_path
-        logging.info(out_table_path)
-
-        # DO whatever and save into out_table_path
-        with open(table.full_path, mode='wt', encoding='utf-8', newline='') as out_file:
-            writer = csv.DictWriter(out_file, fieldnames=['timestamp'])
-            writer.writeheader()
-            writer.writerow({"timestamp": datetime.now().isoformat()})
-
-        # Save table manifest (output.csv.manifest) from the tabledefinition
-        self.write_manifest(table)
-
-        # Write new state - will be available next run
-        self.write_state_file({"some_state_parameter": "value"})
-
-        # ####### EXAMPLE TO REMOVE END
-
+        
+        response=trigger_job(url,username, password, process_id, atom_id)
+        if response:
+            print(response)
+        #self.write_manifest(out_table)
 
 """
         Main entrypoint
